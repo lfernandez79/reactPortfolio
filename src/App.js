016fbfae3ff010979fcd1485fb9447a42af7b49f
LeoFdz <lfernandez79@gmail.com>
2020-04-28 17:46:34 -0500

    added new file for navar, created NavBar function and render to DOM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import HelloReact from "./components/HelloReact";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <HelloReact />;
+  return <NavBar />;
 }
 
 export default App;
