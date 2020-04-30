ba9d5e1942909f3b4ea28879570764ef2c19f455
LeoFdz <lfernandez79@gmail.com>
2020-04-29 20:16:15 -0500

    added wave to letter

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import NavBar from "./components/NavBar";
 
 
+
 function App() {
   return (
     <div>
       <NavBar />
+ 
 
     </div>
   )
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,30 +3,25 @@ import { Wave } from "react-animated-text";
 
 import "./style.css";
 
-
-  
-
-
-
 function NavBar() {
   return (
     <header className="container-fluid row m-0">
 
       <div className="col-sm-12 col-md-6 py-2">
         <h3 className="d-flex justify-content-center align-items-center"><i className="fab fa-css3-alt fa-lg"></i>Leo Fernandez</h3>
-        <Wave text="LOADING DATA" effect="stretch" effectChange={2.0} />
+
       </div>
 
       <nav className="col-sm-12 col-md-6 py-2">
         <ul className="nav d-flex justify-content-center align-items-center">
           <li className="nav-item">
-            <a className="nav-link" href="index.html"><Wave text="A b o u t" effect="stretch" effectChange={2.0}/></a>
+            <a className="nav-link" href="index.html"><Wave text="A b o u t" effect="stretch" effectChange={2.0} /></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="portfolio.html">P o r t f o l i o</a>
+            <a className="nav-link" href="portfolio.html"><Wave text="P o r t f o l i o" effect="stretch" effectChange={2.0} /></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="contact.html">C o n t a c t</a>
+            <a className="nav-link" href="contact.html"><Wave text="C o n t a c t" effect="stretch" effectChange={2.0} /></a>
           </li>
         </ul>
       </nav>
