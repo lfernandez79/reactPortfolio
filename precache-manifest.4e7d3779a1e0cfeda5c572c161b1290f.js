04f5a29cebbe8f6a997a18560ab0c4da66eda226
LeoFdz <lfernandez79@gmail.com>
2020-05-08 16:27:29 -0500

    Updates

---
+++ precache-manifest.4e7d3779a1e0cfeda5c572c161b1290f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7329075b9a273e14edf4ea17d5708811",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "8eeb22581bea47979f01",
+    "url": "/reactPortfolio/static/css/main.4cef4f77.chunk.css"
+  },
+  {
+    "revision": "17534e6e8da2c2ba7680",
+    "url": "/reactPortfolio/static/js/2.f9e3fc1c.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/reactPortfolio/static/js/2.f9e3fc1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eeb22581bea47979f01",
+    "url": "/reactPortfolio/static/js/main.0a4babcf.chunk.js"
+  },
+  {
+    "revision": "f40f85759aed7178a9bd",
+    "url": "/reactPortfolio/static/js/runtime-main.c00b2187.js"
+  },
+  {
+    "revision": "d9a856e6a6f04224f421735334505406",
+    "url": "/reactPortfolio/static/media/Leo.d9a856e6.jpg"
+  },
+  {
+    "revision": "fe95ab8349c68aa6a102258a75f5ede6",
+    "url": "/reactPortfolio/static/media/blogProject.fe95ab83.png"
+  },
+  {
+    "revision": "b7b7f3c670a4ca61797fd539edb29600",
+    "url": "/reactPortfolio/static/media/pizzaProject.b7b7f3c6.png"
+  },
+  {
+    "revision": "bad1cc5f68900bca3c674d781d571335",
+    "url": "/reactPortfolio/static/media/travelAdvisor.bad1cc5f.png"
+  }
+]);
