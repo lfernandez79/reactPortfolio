0469f82b4c30d7d5ae8db56b908b45e3ac7718dc
LeoFdz <lfernandez79@gmail.com>
2020-06-01 14:26:43 -0500

    fixed ppicture for projects

--- src/pages/Projetcs/Projects.js
+++ src/pages/Projetcs/Projects.js
@@ -8,7 +8,7 @@ function Projects() {
     return (
         <div id="Projtcs">
 
-            <h2 id="text3d" className="text-center">Team Projects</h2>
+            <h2 id="text3d" className="text-center">Coding</h2>
 
             <article id="view" className="container-lg my-3">
 
@@ -17,22 +17,19 @@ function Projects() {
                         <div data-aos="fade-up" className="col-xs-12 col-md-6 col-lg-4">
                             <a href="https://lfernandez79.github.io/project1/"><img className="d-flex img-fluid mb-2" src={TravelAdvisor} alt="TravelAdvisor"></img></a>
                             <div className="d-flex justify-content-around">
-                                <button type="button" className="btn btn-outline-dark btn-sm">Respository</button>
-                                <button type="button" className="btn btn-outline-dark btn-sm">Deployed</button>
+                                <button type="button" className="btn btn-outline-dark btn-sm">Live</button>                           
                             </div>
                         </div>
                         <div data-aos="fade-down" className="col-xs-12 col-md-6 col-lg-4">
                             <a href="https://project2-cody-gen-leo.herokuapp.com/"><img className="d-flex img-fluid mb-2" src={Blog} alt="BLog"></img></a>
                             <div className="d-flex justify-content-around">
-                                <button type="button" className="btn btn-outline-dark btn-sm">Respository</button>
-                                <button type="button" className="btn btn-outline-dark btn-sm">Deployed</button>
+                                <button type="button" className="btn btn-outline-dark btn-sm">Live</button>                              
                             </div>
                         </div>
                         <div data-aos="fade-left" className="col-xs-12 col-md-6 col-lg-4">
                             <a href="https://pizza-app-p3.herokuapp.com/"><img className="d-flex img-fluid mb-2" src={Pizza} alt="PizzaPage"></img></a>
                             <div className="d-flex justify-content-around">
-                                <button type="button" className="btn btn-outline-dark btn-sm">Respository</button>
-                                <button type="button" className="btn btn-outline-dark btn-sm">Deployed</button>
+                                <button type="button" className="btn btn-outline-dark btn-sm">Live</button>
                             </div>
                         </div>
                     </div>
