f89d9748cfa94edb5e9c7f1a51c36cec8ad85262
LeoFdz <lfernandez79@gmail.com>
2020-06-01 14:53:29 -0500

    added rreact portfolio

--- src/pages/Projetcs/Projects.js
+++ src/pages/Projetcs/Projects.js
@@ -2,6 +2,8 @@ import React from "react";
 import TravelAdvisor from "../images/travelAdvisor.png"
 import Blog from "../images/blogProject.png"
 import Pizza from "../images/pizzaProject.png"
+import Portfoliov1 from "../images/Portfoliov1.0.png"
+import Portfolio from"../images/Portfoliov2.0.png"
 import "./Projects.css"
 
 function Projects() {
@@ -17,19 +19,31 @@ function Projects() {
                         <div data-aos="fade-up" className="col-xs-12 col-md-6 col-lg-4">
                             <a href="https://lfernandez79.github.io/project1/"><img className="d-flex img-fluid mb-2" src={TravelAdvisor} alt="TravelAdvisor"></img></a>
                             <div className="d-flex justify-content-around">
-                                <button type="button" className="btn btn-outline-dark btn-sm">Live</button>                           
+                                <button type="button" className="btn btn-outline-dark btn-sm">Repository</button>                           
                             </div>
                         </div>
                         <div data-aos="fade-down" className="col-xs-12 col-md-6 col-lg-4">
                             <a href="https://project2-cody-gen-leo.herokuapp.com/"><img className="d-flex img-fluid mb-2" src={Blog} alt="BLog"></img></a>
                             <div className="d-flex justify-content-around">
-                                <button type="button" className="btn btn-outline-dark btn-sm">Live</button>                              
+                                <button type="button" className="btn btn-outline-dark btn-sm">Repository</button>                              
                             </div>
                         </div>
                         <div data-aos="fade-left" className="col-xs-12 col-md-6 col-lg-4">
                             <a href="https://pizza-app-p3.herokuapp.com/"><img className="d-flex img-fluid mb-2" src={Pizza} alt="PizzaPage"></img></a>
                             <div className="d-flex justify-content-around">
-                                <button type="button" className="btn btn-outline-dark btn-sm">Live</button>
+                                <button type="button" className="btn btn-outline-dark btn-sm">Repository</button>
+                            </div>
+                        </div>
+                        <div data-aos="fade-up" className="col-xs-12 col-md-6 col-lg-4 mt-3">
+                            <a href="https://lfernandez79.github.io/leoPortfolio/"><img className="d-flex img-fluid mb-2" src={Portfoliov1} alt="Static Portfolio"></img></a>
+                            <div className="d-flex justify-content-around">
+                                <button type="button" className="btn btn-outline-dark btn-sm">Repository</button>
+                            </div>
+                        </div>
+                        <div data-aos="fade-down" className="col-xs-12 col-md-6 col-lg-4 mt-3">
+                            <a href="https://lfernandez79.github.io/reactPortfolio/"><img className="d-flex img-fluid mb-2" src={Portfolio} alt="React Portfolio"></img></a>
+                            <div className="d-flex justify-content-around">
+                                <button type="button" className="btn btn-outline-dark btn-sm">Repository</button>
                             </div>
                         </div>
                     </div>
