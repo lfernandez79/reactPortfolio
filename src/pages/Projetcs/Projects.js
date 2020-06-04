daff7eeea6f15df904cdbdd9b40579f91063a438
LeoFdz <lfernandez79@gmail.com>
2020-06-04 10:53:52 -0500

    added Style-it npm for inline css style

--- src/pages/Projetcs/Projects.js
+++ src/pages/Projetcs/Projects.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Style from "style-it";
 import TravelAdvisor from "../images/travelAdvisor.png"
 import Blog from "../images/blogProject.png"
 import Pizza from "../images/pizzaProject.png"
 import Portfoliov1 from "../images/Portfoliov1.0.png"
 import Portfolio from"../images/Portfoliov2.0.png"
 import "./Projects.css"
+
 
 function Projects() {
     return (
@@ -16,12 +18,28 @@ function Projects() {
 
                 <main>                   
                     <div className="row pt-2 px-3">
-                        <div data-aos="fade-up" className="col-xs-12 col-md-6 col-lg-4 mt-4">
-                            <a href="https://lfernandez79.github.io/project1/"><img className="d-flex img-fluid mb-2" src={TravelAdvisor} alt="TravelAdvisor"></img></a>
-                            <div className="d-flex justify-content-around">
-                                                           
+                        <Style>{
+                        
+                        `img:hover {
+                            position: absolute;
+                            top: 0;
+                            bottom: 0;
+                            left: 0;
+                            right: 0;
+                            height: 100%;
+                            width: 100%;
+                            opacity: 0;
+                            transition: .5s ease;
+                            background-color: "blue";
+
+                        }`}
+                            <div data-aos="fade-up" className="col-xs-12 col-md-6 col-lg-4 mt-4">
+                                <a href="https://lfernandez79.github.io/project1/"><img className="d-flex img-fluid mb-2" src={TravelAdvisor} alt="TravelAdvisor"></img></a>
+                                <div className="d-flex justify-content-around">
+                                                            
+                                </div>
                             </div>
-                        </div>
+                        </Style>
                         <div data-aos="fade-down" className="col-xs-12 col-md-6 col-lg-4 mt-4">
                             <a href="https://project2-cody-gen-leo.herokuapp.com/"><img className="d-flex img-fluid mb-2" src={Blog} alt="BLog"></img></a>
                             <div className="d-flex justify-content-around">
