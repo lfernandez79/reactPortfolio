7ce90e773d8ad9572008958a5988b87981111198
LeoFdz <lfernandez79@gmail.com>
2020-06-05 08:14:38 -0500

    added fade  zoom 80% works

--- src/pages/Projetcs/Projects.js
+++ src/pages/Projetcs/Projects.js
@@ -5,7 +5,7 @@ import Blog from "../images/blogProject.png"
 import Pizza from "../images/pizzaProject.png"
 import Portfoliov1 from "../images/Portfoliov1.0.png"
 import Portfolio from "../images/Portfoliov2.0.png"
-import "./Projects.css"
+
 
 
 function Projects() {
@@ -20,42 +20,59 @@ function Projects() {
                     <div className="row pt-2 px-3">
                         <Style>
                         {`
-                        
-                            .hidden {
+                            
+                            .blue-bkg-text {
+                                opacity: 1;
                                 background-color: #008CBA;
+                                
                             }
-                            .hidden:hover {                              
+                            
+                            img:hover {
                                 opacity: 0;
-                                }
+                                transition: .5s ease;
+                            }
+                           
+                            .text {
+                                color: white;
+                                font-size: 20px;
+                                position: absolute;
+                                top: 50%;
+                                left: 50%;
+                                -webkit-transform: translate(-50%, -50%);
+                                -ms-transform: translate(-50%, -50%);
+                                transform: translate(-50%, -50%);
+                                text-align: center;
+                            }
+                            
                         
                         `}
-                            <div data-aos="fade-up" className="hidden col-xs-12 col-md-6 col-lg-4 mt-4">
-                                <a href="https://lfernandez79.github.io/project1/"><img className="d-flex img-fluid mb-2" src={TravelAdvisor} alt="TravelAdvisor"></img></a>
+                            <div data-aos="fade-up" className="blue-bkg-text col-xs-12 col-md-6 col-lg-4 p-0">
+                                <a href="https://lfernandez79.github.io/project1/"><img className="d-flex img-fluid" src={TravelAdvisor} alt="TravelAdvisor"></img></a>
                                 <div className="overlay d-flex justify-content-around"> 
-                                                                       
+                                     <div className="text">Some text</div>                                  
                                 </div>
                             </div>
                         </Style>
-                        <div data-aos="fade-down" className="col-xs-12 col-md-6 col-lg-4 mt-4">
-                            <a href="https://project2-cody-gen-leo.herokuapp.com/"><img className="d-flex img-fluid mb-2" src={Blog} alt="BLog"></img></a>
+                        <div data-aos="fade-down" className="col-xs-12 col-md-6 col-lg-4">
+                            <a href="https://project2-cody-gen-leo.herokuapp.com/"><img className="d-flex img-fluid" src={Blog} alt="BLog"></img></a>
                             <div className="d-flex justify-content-around">
 
                             </div>
                         </div>
-                        <div data-aos="fade-left" className="col-xs-12 col-md-6 col-lg-4 mt-4">
-                            <a href="https://pizza-app-p3.herokuapp.com/"><img className="d-flex img-fluid mb-2" src={Pizza} alt="PizzaPage"></img></a>
+                        <div data-aos="fade-left" className="col-xs-12 col-md-6 col-lg-4">
+                            <a href="https://pizza-app-p3.herokuapp.com/"><img className="d-flex img-fluid" src={Pizza} alt="PizzaPage"></img></a>
                             <div className="d-flex justify-content-around">
 
                             </div>
                         </div>
-                        <div data-aos="fade-up" className="col-xs-12 col-md-6 col-lg-4 mt-4">
-                            <a href="https://lfernandez79.github.io/leoPortfolio/"><img className="d-flex img-fluid mb-2" src={Portfoliov1} alt="Static Portfolio"></img></a>
+                        <div data-aos="fade-up" className="col-xs-12 col-md-6 col-lg-4">
+                            <a href="https://lfernandez79.github.io/leoPortfolio/"><img className="d-flex img-fluid" src={Portfoliov1} alt="Static Portfolio"></img></a>
                             <div className="d-flex justify-content-around">
 
                             </div>
                         </div>
-                        <div data-aos="fade-down" className="col-xs-12 col-md-6 col-lg-4 mt-4">
-                            <a href="https://lfernandez79.github.io/reactPortfolio/"><img className="d-flex img-fluid mb-2" src={Portfolio} alt="React Portfolio"></img></a>
+                        <div data-aos="fade-down" className="col-xs-12 col-md-6 col-lg-4">
+                            <a href="https://lfernandez79.github.io/reactPortfolio/"><img className="d-flex img-fluid" src={Portfolio} alt="React Portfolio"></img></a>
                             <div className="d-flex justify-content-around">
 
                             </div>
