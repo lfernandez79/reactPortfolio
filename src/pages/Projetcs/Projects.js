573656d18752a6da32bbc8a8049ab8bb54fbba24
LeoFdz <lfernandez79@gmail.com>
2020-06-05 11:58:34 -0500

    added more style with inline styling

--- src/pages/Projetcs/Projects.js
+++ src/pages/Projetcs/Projects.js
@@ -21,32 +21,35 @@ function Projects() {
                     <div className="row pt-2 px-3">
 
                         <div data-aos="fade-up" className="container col-xs-12 col-md-6 col-lg-4 p-0">
-                            <a href="https://lfernandez79.github.io/project1/"><img className="d-flex img-fluid" src={TravelAdvisor} alt="TravelAdvisor"></img></a>
+                            <img className="d-flex img-fluid" src={TravelAdvisor} alt="TravelAdvisor"></img>
                             <div className="overlay">
-                                <div className="text">Some text</div>
+                                <p style={{textAlign: "center"}}>Some Text</p>
+                                <p className="text"><a href="https://lfernandez79.github.io/project1/" style={{textDecoration: "none" }}>Live </a>
+                                <a href="https://lfernandez79.github.io/project1/" style={{textDecoration: "none" }}> Code</a></p> 
+                                
                             </div>
                         </div>
 
-                        <div data-aos="fade-down" className="container col-xs-12 col-md-6 col-lg-4">
+                        <div data-aos="fade-down" className="container col-xs-12 col-md-6 col-lg-4 p-0">
                             <a href="https://project2-cody-gen-leo.herokuapp.com/"><img className="d-flex img-fluid" src={Blog} alt="BLog"></img></a>
                             <div className="overlay">
                                 <div className="text">Some text</div>
                             </div>
                         </div>
 
-                        <div data-aos="fade-left" className="container col-xs-12 col-md-6 col-lg-4">
+                        <div data-aos="fade-left" className="container col-xs-12 col-md-6 col-lg-4 p-0">
                             <a href="https://pizza-app-p3.herokuapp.com/"><img className="d-flex img-fluid" src={Pizza} alt="PizzaPage"></img></a>
                             <div className="overlay">
                                 <div className="text">Some text</div>
                             </div>
                         </div>
-                        <div data-aos="fade-up" className="container col-xs-12 col-md-6 col-lg-4">
+                        <div data-aos="fade-up" className="container col-xs-12 col-md-6 col-lg-4 p-0">
                             <a href="https://lfernandez79.github.io/leoPortfolio/"><img className="d-flex img-fluid" src={Portfoliov1} alt="Static Portfolio"></img></a>
                             <div className="overlay">
                                 <div className="text">Some text</div>
                             </div>
                         </div>
-                        <div data-aos="fade-down" className="container col-xs-12 col-md-6 col-lg-4">
+                        <div data-aos="fade-down" className="container col-xs-12 col-md-6 col-lg-4 p-0">
                             <a href="https://lfernandez79.github.io/reactPortfolio/"><img className="d-flex img-fluid" src={Portfolio} alt="React Portfolio"></img></a>
                             <div className="overlay">
                                 <div className="text">Some text</div>
