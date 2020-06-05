0c817372cd9c81ed87559d58b2a4b3712e0a206b
LeoFdz <lfernandez79@gmail.com>
2020-06-05 16:50:44 -0500

    fixed fade and text when hover with spread operator

--- src/pages/Projetcs/Projects.js
+++ src/pages/Projetcs/Projects.js
@@ -6,21 +6,30 @@ import Portfoliov1 from "../images/Portfoliov1.0.png"
 import Portfolio from "../images/Portfoliov2.0.png"
 import "./Projects.css"
 
+const style = {
+    font: {
+        fontSize: "2rem"
+    },
+    deco: {
+        textDecorationLine: "none"
+    },
+}
+
 function Projects() {
     return (
         <div>
             <h2 className="text-center">Coding</h2>
-            
+
             <article id="view" className="container-lg my-3">
                 <main>
                     <div className="row pt-2 px-3">
+
                         <div data-aos="fade-up" className="container col-xs-12 col-md-6 col-lg-4 p-0">
                             <img className="d-flex img-fluid" src={TravelAdvisor} alt="TravelAdvisor"></img>
                             <div className="overlay">
-                                <p style={{ textAlign: "center" }}>Some Text</p>
-                                <p className="text"><a href="https://lfernandez79.github.io/project1/" style={{ textDecoration: "none" }}>Live </a>
-                                    <a href="https://lfernandez79.github.io/project1/" style={{ textDecoration: "none" }}> Code</a></p>
-
+                                <p className="text1" style={style.font}>Some Text</p>
+                                <p className="text2"><a href="https://lfernandez79.github.io/project1/" style={{ ...style.font, ...style.deco }}>Live </a>
+                                    <a href="https://lfernandez79.github.io/project1/" style={{ ...style.font, ...style.deco }}> GitHub</a></p>
                             </div>
                         </div>
 
@@ -37,12 +46,14 @@ function Projects() {
                                 <div className="text">Some text</div>
                             </div>
                         </div>
+
                         <div data-aos="fade-up" className="container col-xs-12 col-md-6 col-lg-4 p-0">
                             <a href="https://lfernandez79.github.io/leoPortfolio/"><img className="d-flex img-fluid" src={Portfoliov1} alt="Static Portfolio"></img></a>
                             <div className="overlay">
                                 <div className="text">Some text</div>
                             </div>
                         </div>
+
                         <div data-aos="fade-down" className="container col-xs-12 col-md-6 col-lg-4 p-0">
                             <a href="https://lfernandez79.github.io/reactPortfolio/"><img className="d-flex img-fluid" src={Portfolio} alt="React Portfolio"></img></a>
                             <div className="overlay">
@@ -51,9 +62,7 @@ function Projects() {
                         </div>
 
                     </div>
-
                 </main>
-
             </article>
         </div>
     );
