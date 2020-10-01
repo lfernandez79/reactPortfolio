b7ac7f133fcf5f63d9e9096131c33f54a61a9a56
LeoFdz <lfernandez79@gmail.com>
2020-10-01 17:29:19 -0500

    fixed index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -13,37 +13,34 @@ const style = {
 
 function NavBar() {
   return (
-    <div id="Navbar">
-      <header className="row align-items-center py-3">
+    <header className="row align-items-center py-3">
 
-        <div className="col-sm-12 col-md-6">
-          <a className="d-flex justify-content-center" href="https://travis-ci.com/github/lfernandez79/reactPortfolio" style={style.removeUnderline}>
-            <img id="radius" src={TravisCI} style={{ width: "5%", height: "5%" }} alt="TravisCI" />
-            <span>
+      <div className="col-sm-12 col-md-6">
+        <a className="d-flex justify-content-center" href="https://travis-ci.com/github/lfernandez79/reactPortfolio" style={style.removeUnderline}>
+          <img id="radius" src={TravisCI} style={{ width: "5%", height: "5%" }} alt="TravisCI" />
+          <span>
             &nbsp; CI/CD!
-            </span>
-          </a>
+          </span>
+        </a>
 
-        </div>
+      </div>
 
-        <nav className="col-sm-12 col-md-6">
-          <ul className="nav d-flex justify-content-around">
-            <li className="nav-item">
-              <Link to="About" duration={500} smooth className="nav-link" href="/"><Wave text="About" effect="stretch" effectChange={2.0} /></Link>
-            </li>
-            <li className="nav-item">
-              <Link to="Projtcs" duration={500} smooth className="nav-link" href="/"><Wave text="Portfolio" effect="stretch" effectChange={2.0} /></Link>
-            </li>
-            <li className="nav-item">
-              <Link to="Contact" duration={500} smooth className="nav-link" href="/"><Wave text="Contact" effect="stretch" effectChange={2.0} /></Link>
-            </li>
-          </ul>
+      <nav className="col-sm-12 col-md-6">
+        <ul className="nav d-flex justify-content-around">
+          <li className="nav-item">
+            <Link to="About" duration={500} smooth className="nav-link" href="/"><Wave text="About" effect="stretch" effectChange={2.0} /></Link>
+          </li>
+          <li className="nav-item">
+            <Link to="Projtcs" duration={500} smooth className="nav-link" href="/"><Wave text="Portfolio" effect="stretch" effectChange={2.0} /></Link>
+          </li>
+          <li className="nav-item">
+            <Link to="Contact" duration={500} smooth className="nav-link" href="/"><Wave text="Contact" effect="stretch" effectChange={2.0} /></Link>
+          </li>
+        </ul>
 
-        </nav>
+      </nav>
 
-      </header>
-      <hr />
-    </div>
+    </header>
   );
 }
 
