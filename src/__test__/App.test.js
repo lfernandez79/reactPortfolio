9cc81fb4a78e223cd4adb07033dd6371c97a8d36
Leo Fernandez <lfernandez79@gmail.com>
2024-02-08 21:36:01 -0600

    fixed the root container by adding createRoot(div) inside root variable

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -5,6 +5,7 @@ import App from "../App";
 // eslint-disable-next-line no-undef
 it("renders without crashing or errors", () => {
   const div = document.createElement("div");
-  createRoot(div).render(<App />);
-  createRoot.unmount();
+  const root = createRoot(div);
+  root.render(<App />);
+  root.unmount(div);
 });
